123f08d8ffdbb9ff42af20bb0322e9ddee05811a
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-05-31 23:37:07 +0530

    Add addition feature

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,1 +1,8 @@
-console.log("This is a Calculator application");
+console.log("This is a Calculator application\n");
+
+function addition(n1:number , n2:number):number{
+    return n1 + n2 ;
+}
+
+const sum = addition(10,20);
+console.log("Addition of 10 and 20 is ",sum);
